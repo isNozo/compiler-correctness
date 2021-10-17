bbd4760346864b24e965bc867c51dc1a35da4afd
nozo <nozomi.pine@gmail.com>
2021-10-17 04:50:25 +0000

    Add total_map

--- IMP.v
+++ IMP.v
@@ -216,7 +216,19 @@ Proof.
       + intros. reflexivity.
 Qed.
 
-Definition state := total_map nat.
+Definition total_map (A : Type) := string -> A.
 
+Definition t_empty {A : Type} (v : A) : total_map A :=
+    (fun _ => v).
+
+Definition t_update {A : Type} (m : total_map A)
+                    (x : string) (v : A) :=
+    fun x' => if eqb_string x x' then v else m x'.
+
+Notation "'_' '!->' v" := (t_empty v)
+    (at level 100, right associativity).
+
+Notation "x '!->' v ';' m" := (t_update m x v)
+    (at level 100, v at next level, right associativity).
 
 End AExp.
